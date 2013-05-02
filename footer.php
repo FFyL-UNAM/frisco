d23d0da2dffab821f024b17d82d5bcae3db07250
Marco Godínez <markotom@gmail.com>
2013-05-01 19:41:20 -0500

    Added divs to footer

--- footer.php
+++ footer.php
@@ -1,14 +1,19 @@
-<ul class="partners">
-  <li class="redhd">
-    <a href="http://www.humanidadesdigitales.net">
-      Humanidades Digitales</a>
-  </li>
-  <li class="hdh">
-    <a href="http://hdh2013.humanidadesdigitales.org">
-      Humanidades Digitales Hispánicas
-    </a>
-  </li>
-</ul>
+
+<div id="bp-before-footer" class="fullwidth">
+	<div id="footer">
+		<ul class="partners">
+		  <li class="redhd">
+		    <a href="http://www.humanidadesdigitales.net">
+		      Humanidades Digitales</a>
+		  </li>
+		  <li class="hdh">
+		    <a href="http://hdh2013.humanidadesdigitales.org">
+		      Humanidades Digitales Hispánicas
+		    </a>
+		  </li>
+		</ul>
+	</div>
+</div>
 
 <?php wp_footer();?>
 
