c378743479f03fe29f4a71e195694a7c4a7eb3c4
Marco Godínez <markotom@gmail.com>
2013-05-01 19:31:38 -0500

    Added partner logos

---
+++ footer.php
@@ -0,0 +1,11 @@
+<ul class="partners">
+  <li class="redhd">
+    <a href="http://www.humanidadesdigitales.net">
+      Humanidades Digitales</a>
+  </li>
+  <li class="hdh">
+    <a href="http://hdh2013.humanidadesdigitales.org">
+      Humanidades Digitales Hispánicas
+    </a>
+  </li>
+</ul>
