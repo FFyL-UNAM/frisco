341ffa1561fc6f10d80768c8cc8aba2daef919fd
Marco Godínez <markotom@gmail.com>
2013-05-06 17:09:25 -0500

    Added all partner logos

--- footer.php
+++ footer.php
@@ -1,23 +1,36 @@
 </div>
 <div id="bp-before-footer" class="fullwidth">
-	<div id="footer">
-		<ul class="partners">
-		  <li class="redhd">
-		    <a href="http://www.humanidadesdigitales.net">
-		      Humanidades Digitales</a>
-		  </li>
-		  <li class="hdh">
-		    <a href="http://hdh2013.humanidadesdigitales.org">
-		      Humanidades Digitales Hispánicas
-		    </a>
-		  </li>
-		  <li class="fcsh">
-		    <a href="http://www.fcsh.unl.pt">
-		      Faculdade de Ciências Sociais e Humanas da Universidade Nova de Lisboa
-		    </a>
-		  </li>
-		</ul>
-	</div>
+  <div id="footer">
+    <ul class="partners">
+      <li class="redhd">
+        <a href="http://www.humanidadesdigitales.net">
+          Humanidades Digitales</a>
+      </li>
+      <li class="hdh">
+        <a href="http://hdh2013.humanidadesdigitales.org">
+          Humanidades Digitales Hispánicas
+        </a>
+      </li>
+      <li class="fcsh">
+        <a href="http://www.fcsh.unl.pt">
+          Faculdade de Ciências Sociais e Humanas da Universidade Nova de Lisboa
+        </a>
+      </li>
+      <li class="fcsh">
+        <a href="http://www.fcsh.unl.pt">
+          Faculdade de Ciências Sociais e Humanas da Universidade Nova de Lisboa
+        </a>
+      </li><li class="usp">
+        <a href="http://www.usp.br">
+          Universidade de São Paulo
+        </a>
+      </li><li class="brasiliana">
+        <a href="http://www.brasiliana.usp.br">
+          Biblioteca Brasiliana Guita e José Mindlin
+        </a>
+      </li>
+    </ul>
+  </div>
 </div>
 
 <?php wp_footer();?>
